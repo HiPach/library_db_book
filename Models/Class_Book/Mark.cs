7601043541a942df241dfc5f2ce0615539447d52
HiPach <kiritvip0@list.ru>
2022-07-12 13:37:20 +0700

    NameMark and NumberShelf измененно на Name

--- Models/Class_Book/Mark.cs
+++ Models/Class_Book/Mark.cs
@@ -6,7 +6,7 @@ namespace library_db_book.Models.Class_Book
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string NameMark { get; set; }
+        public string Name { get; set; }
         public IList<BookMarkRelation> BookMark { get; set; }
     }
 }
